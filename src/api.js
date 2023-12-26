d8a8d2d59a0c8967b4758a4d84358d708e64d609
KARTHIK LAGVANKAR <77400421+LagvankarKarthik@users.noreply.github.com>
2023-12-26 10:20:14 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c1fc0e542dmsh870c2ccbbad2fe4p13581djsnb878295108f6",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "e251bd074620bcbdaf5d4168c05ba9bd";
